b3fd86f654110d360077aa0cb46108180ae3700c
Pepe Valdivia <jvaldivia@softweb.pe>
2018-12-17 17:01:27 -0500

    Login form

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,13 +1,25 @@
 class LoginController < ApplicationController
   include LoginHelper
+
   def managment
     @locals = {
       :title => 'Bienvenido',
       :message => '',
       :message_type => '',
-      :css => LoginHelper::index_css,
-      :js => LoginHelper::index_js,
+      :css => LoginHelper::managment_css,
+      :js => LoginHelper::managment_js,
     }
     render template: 'login/managment', layout: 'blank'
+  end
+
+  def index
+    @locals = {
+      :title => 'Bienvenido',
+      :message => '',
+      :message_type => '',
+      :css => LoginHelper::index_css,
+      :js => LoginHelper::managment_js,
+    }
+    render template: 'login/index', layout: 'blank'
   end
 end
--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,5 +1,5 @@
 module LoginHelper
-  def self.index_css
+  def self.managment_css
     rpta = nil
     if CONSTANTS[:ambiente] == 'desarrollo'
       rpta = [
@@ -7,8 +7,23 @@ module LoginHelper
         CONSTANTS[:static_url] + 'bower_components/font-awesome/css/font-awesome.min',
         CONSTANTS[:static_url] + 'bower_components/swp-backbone/assets/css/constants',
         CONSTANTS[:static_url] + 'bower_components/swp-backbone/assets/css/login',
-        CONSTANTS[:static_url] + 'assets/css/constants',
-        CONSTANTS[:static_url] + 'assets/css/login',
+        CONSTANTS[:static_url] + 'assets/css/constants-managment',
+        CONSTANTS[:static_url] + 'assets/css/login-managment',
+      ]
+    else
+      rpta = [
+        CONSTANTS[:static_url] + 'dist/login-managment.min',
+      ]
+    end
+    rpta
+  end
+
+  def self.managment_js
+    rpta = nil
+    if CONSTANTS[:ambiente] == 'desarrollo'
+      rpta = [
+        CONSTANTS[:static_url] + 'bower_components/jquery/dist/jquery.min',
+        CONSTANTS[:static_url] + 'bower_components/bootstrap/dist/js/bootstrap.min',
       ]
     else
       rpta = [
@@ -18,12 +33,16 @@ module LoginHelper
     rpta
   end
 
-  def self.index_js
+  def self.index_css
     rpta = nil
     if CONSTANTS[:ambiente] == 'desarrollo'
       rpta = [
-        CONSTANTS[:static_url] + 'bower_components/jquery/dist/jquery.min',
-        CONSTANTS[:static_url] + 'bower_components/bootstrap/dist/js/bootstrap.min',
+        CONSTANTS[:static_url] + 'bower_components/bootstrap/dist/css/bootstrap.min',
+        CONSTANTS[:static_url] + 'bower_components/font-awesome/css/font-awesome.min',
+        CONSTANTS[:static_url] + 'bower_components/swp-backbone/assets/css/constants',
+        CONSTANTS[:static_url] + 'assets/css/constants',
+        CONSTANTS[:static_url] + 'assets/css/styles',
+        CONSTANTS[:static_url] + 'assets/css/login',
       ]
     else
       rpta = [
