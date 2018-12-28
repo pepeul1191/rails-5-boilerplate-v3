d9aab31c2628d23fe98b451fc54d11ebfaf0d67c
Pepe Valdivia <jvaldivia@softweb.pe>
2018-12-28 17:12:05 -0500

    Manejando error de password_controller

--- app/controllers/access/password_controller.rb
+++ app/controllers/access/password_controller.rb
@@ -35,31 +35,36 @@ class Access::PasswordController < ApplicationController
     if params[:pass1] == '' || params[:pass2] == ''
       message = 'Debe ingrear ambas contraseñas'
     elsif params[:pass1] == params[:pass2] && params[:pass1] != ''
-      message_type = 'color-success'
-      # cambiar key
-      key = Access::UserKey.where(
-        :user_id => params[:user_id],
-        :activation => params[:activation]
-      ).first()
-      key.activation = Assets::Randito.string_number(25)
-      key.save
-      # actualizar password de user
-      user = Access::User.where(
-        :id => params[:user_id],
-      ).first()
-      user.pass = Assets::Cipher.encrypt(CONSTANTS[:key], params[:pass1])
-      user.user_state_id = 1
-      user.save
-      # render view
-      contents = get_content('password/create')[lang]
-      @locals = {
-        :title => get_titles()[lang]['password_create_ok'],
-        :contents => contents,
-        :css => Access::PasswordHelper::create_css_ok,
-        :js => Access::PasswordHelper::create_js,
-      }
-      render template: 'access/password/create_ok', layout: 'blank'
-      return nil
+      begin
+        message_type = 'color-success'
+        # cambiar key
+        key = Access::UserKey.where(
+          :user_id => params[:user_id],
+          :activation => params[:activation]
+        ).first()
+        key.activation = Assets::Randito.string_number(25)
+        key.save
+        # actualizar password de user
+        user = Access::User.where(
+          :id => params[:user_id],
+        ).first()
+        user.pass = Assets::Cipher.encrypt(CONSTANTS[:key], params[:pass1])
+        user.user_state_id = 1
+        user.save
+        # render view
+        contents = get_content('password/create')[lang]
+        @locals = {
+          :title => get_titles()[lang]['password_create_ok'],
+          :contents => contents,
+          :css => Access::PasswordHelper::create_css_ok,
+          :js => Access::PasswordHelper::create_js,
+        }
+        render template: 'access/password/create_ok', layout: 'blank'
+        return nil
+      rescue Exception => e
+        puts e.backtrace
+        message = 'Ocurrió un error en crear la contraseña'
+      end
     else
       message = 'Contraseñas no coniciden'
     end
@@ -113,31 +118,36 @@ class Access::PasswordController < ApplicationController
     if params[:pass1] == '' || params[:pass2] == ''
       message = 'Debe ingrear ambas contraseñas'
     elsif params[:pass1] == params[:pass2] && params[:pass1] != ''
-      message_type = 'color-success'
-      # cambiar key
-      key = Access::UserKey.where(
-        :user_id => params[:user_id],
-        :reset => params[:reset]
-      ).first()
-      key.reset = Assets::Randito.string_number(25)
-      key.save
-      # actualizar password de user
-      user = Access::User.where(
-        :id => params[:user_id],
-      ).first()
-      user.pass = Assets::Cipher.encrypt(CONSTANTS[:key], params[:pass1])
-      user.user_state_id = 1
-      user.save
-      # render view
-      contents = get_content('password/create')[lang]
-      @locals = {
-        :title => get_titles()[lang]['password_create_ok'],
-        :contents => contents,
-        :css => Access::PasswordHelper::create_css_ok,
-        :js => Access::PasswordHelper::create_js,
-      }
-      render template: 'access/password/create_ok', layout: 'blank'
-      return nil
+      begin
+        message_type = 'color-success'
+        # cambiar key
+        key = Access::UserKey.where(
+          :user_id => params[:user_id],
+          :reset => params[:reset]
+        ).first()
+        key.reset = Assets::Randito.string_number(25)
+        key.save
+        # actualizar password de user
+        user = Access::User.where(
+          :id => params[:user_id],
+        ).first()
+        user.pass = Assets::Cipher.encrypt(CONSTANTS[:key], params[:pass1])
+        user.user_state_id = 1
+        user.save
+        # render view
+        contents = get_content('password/create')[lang]
+        @locals = {
+          :title => get_titles()[lang]['password_create_ok'],
+          :contents => contents,
+          :css => Access::PasswordHelper::create_css_ok,
+          :js => Access::PasswordHelper::create_js,
+        }
+        render template: 'access/password/create_ok', layout: 'blank'
+        return nil
+      rescue Exception => e
+        puts e.backtrace
+        message = 'Ocurrió un error en actualizar la contraseña'
+      end
     else
       message = 'Contraseñas no coniciden'
     end
